65a50229399b30305883b6b4a9fff474f85bcc0e
Rameez Israr <rameez@govassist.com>
2025-07-09 15:58:22 +0500

    Add server action and pages to create invoices

---
+++ app/lib/action.ts
@@ -0,0 +1,53 @@
+'use server';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+//validation with zod
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(), // convert string to integer and also validating
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      });
+
+    //It's usually good practice to store monetary values 
+    //in cents in your database to eliminate JavaScript
+    // floating-point errors and ensure greater accuracy.
+    const amountInCents = amount * 100;
+    
+    const date = new Date().toISOString().split('T')[0];
+
+    //insert into database
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    //remove cache to get latest changes
+    revalidatePath('/dashboard/invoices');
+
+    redirect('/dashboard/invoices');
+
+    // const rawFormData = {
+    //   customerId: formData.get('customerId'),
+    //   amount: formData.get('amount'),
+    //   status: formData.get('status'),
+    // };
+    // console.log(rawFormData);
+    // console.log(typeof rawFormData.amount);
+  }
